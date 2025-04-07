41d8c88641b6869e788377af5af8f9f3efe577d9
AngelDaelBautistaResendiz <resendizdael@gmail.com>
2025-04-07 15:54:32 -0600

    codigo finalizado

--- demo.py
+++ demo.py
@@ -6,3 +6,10 @@ class Cuadrado:
      if self.lado == 0:
         return 0
      return self.lado * self.lado 
+
+##################################
+lado = int(input("ingresa el valor del lado: "))
+mi_ejemplo = Cuadrado(lado)
+total = mi_ejemplo.area
+
+print(f"el area es {total}") 
