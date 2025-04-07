63a484e627a7a27e506f601ad2ed7e718b7fc5f7
AngelDaelBautistaResendiz <resendizdael@gmail.com>
2025-04-07 15:41:34 -0600

    clase finalizada

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
 class Cuadrado:
-    pass
+    def __init__(self, lado):
+        self.lado = lado
+    
+    def area(self):
+     if self.lado == 0:
+        return 0
+     return self.lado * self.lado 
